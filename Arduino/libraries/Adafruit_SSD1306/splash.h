1663ce47961a1e5aa1af3d8f2f35dd15b9483bd9
fmpaci <fmpaci88@gmail.com>
2019-06-25 22:35:28 -0300

    First Release

---
+++ Arduino/libraries/Adafruit_SSD1306/splash.h
@@ -0,0 +1,14 @@
+#define splash1_width  1
+#define splash1_height 1
+
+const uint8_t PROGMEM splash1_data[] = {
+0x00
+};
+
+
+#define splash2_width  115
+#define splash2_height  32
+
+const uint8_t PROGMEM splash2_data[] = {
+  0x00
+  };
